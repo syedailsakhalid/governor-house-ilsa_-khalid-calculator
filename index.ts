0aac45a621e859f45ea32f3d9302ac7242fb252f
syedailsakhalid <160718759+syedailsakhalid@users.noreply.github.com>
2024-03-18 02:20:49 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter firstnumber", type: "number", name: "firstnumber" },
+  { message: "Enter secondnumber", type: "number", name: "secondnumber" },
+  {
+    message: "select one of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices:["Addision", "Subtraction", "Multplication", "Division"],
+  },
+]);
+
+//console.log(answer);
+
+//conditional statement
+if(answer.operator==="Addision"){
+    console.log( answer.firstnumber + answer.secondnumber)
+}else if(answer.operator==="Subtraction"){
+    console.log( answer.firstnumber - answer.secondnumber)
+}else if(answer.operator==="Multplication"){
+    console.log( answer.firstnumber * answer.secondnumber)
+}else if(answer.operator==="Division"){
+    console.log( answer.firstnumber / answer.secondnumber)
+}
+else{console.log("you are some where wrong")}
